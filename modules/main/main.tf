a9af9b0f212d446edd21257ed9fb49228436b0d8
Alex <5962834-songspire@users.noreply.gitlab.com>
2021-02-04 09:21:11 +0100

    commented out instance and fw resources in main module

--- modules/main/main.tf
+++ modules/main/main.tf
@@ -5,7 +5,7 @@ variable "env" {}
 
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    # version = "0.6.0"
+    version = "~> 3.0"
 
     project_id   = var.project_id
     network_name = var.env
@@ -19,3 +19,57 @@ module "vpc" {
 
     ]
 }
+
+# module "startup-script-lib" {
+#   source = "git::https://github.com/terraform-google-modules/terraform-google-startup-scripts.git?ref=v0.1.0"
+# }
+
+# resource "google_compute_instance" "jump" {
+#   allow_stopping_for_update = true
+#   name         = "jump"
+#   machine_type = "e2-small"
+#   zone         = var.zone
+
+#   tags = ["jump"]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-10"
+#     }
+#   }
+
+#   // Local SSD disk
+#   # scratch_disk {
+#   #   interface = "SCSI"
+#   # }
+
+#   network_interface {
+#     network    = module.vpc.network_name
+#     subnetwork = element(module.vpc.subnets_names, 0)
+
+#     access_config {
+#       // Ephemeral IP
+#     }
+#   }
+
+# #   metadata_startup_script = file("bootstrap.sh")
+
+#   service_account {
+#     scopes = ["userinfo-email", "compute-rw", "storage-rw"]
+#   }
+# }
+
+# resource "google_compute_firewall" "jump" {
+#   name    = "jump"
+#   network = module.vpc.network_name
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
+
+# #   source_ranges = ["8.8.8.8/32"]
+#   target_tags   = ["jump"]
+# }
+
+
